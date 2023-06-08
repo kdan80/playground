72d44cc076990b4d280ba6dc7c50c677fa9efcad
kdan80 <kierandansey80@gmail.com>
2023-06-08 15:31:37 +0100

    Finally got laravel + inertia + react working

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,13 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.jsx')
+    
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
     @inertiaHead
   </head>
   <body>
     @inertia
+
   </body>
 </html>
